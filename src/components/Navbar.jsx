c648fc38ce97a192ed546d8d6f865a1d27131a76
Thanaporn Sae-pueng <78015751+ploy-thanaporn@users.noreply.github.com>
2022-06-14 13:46:15 +0700

    update nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -90,11 +90,11 @@ const Navbar = () => {
             : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
         }
       >
-        <li className="py-6 text-4xl">Home</li>
-        <li className="py-6 text-4xl">About</li>
-        <li className="py-6 text-4xl">Skills</li>
-        <li className="py-6 text-4xl">Experince</li>
-        <li className="py-6 text-4xl">Contact</li>
+        <li className="py-6 text-xl">Home</li>
+        <li className="py-6 text-xl">About</li>
+        <li className="py-6 text-xl">Skills</li>
+        <li className="py-6 text-xl">Experince</li>
+        <li className="py-6 text-xl">Contact</li>
       </ul>
 
       {/* social icons */}
